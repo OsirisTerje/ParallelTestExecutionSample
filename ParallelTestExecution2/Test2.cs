05b620715639cc94939a6d37c4fe06876717c17d
Terje Sandstrom <terje@hermit.no>
2016-01-11 11:50:01 +0100

    updated

---
+++ ParallelTestExecution2/Test2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ParallelTestExecution2
+{
+    public class Test2
+    {
+        [Test]
+        public void Test3Secs2()
+        {
+            Thread.Sleep(5000);
+            Assert.IsTrue(true);
+        }
+    }
+}
