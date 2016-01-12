2e96fefcdd293d49baa6e5b9398bc8edb1b0b497
Terje Sandstrom <terje@hermit.no>
2016-01-12 22:26:48 +0100

    Using NUnit project types

---
+++ NUnit.Tests5/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests5
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Test5Secs()
+        {
+            var x = 5.6d;
+            var s = Stopwatch.StartNew();
+            s.Start();
+            while (s.ElapsedMilliseconds < 5000)
+            {
+                for (int i = 0; i < 2000000; i++)
+                {
+                    x = x * i / (Math.Sqrt(i));
+                }
+            }
+            Assert.IsTrue(true);
+        }
+    }
+}
