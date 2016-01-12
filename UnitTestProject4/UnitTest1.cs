694821460068f579e9775febe572a2707901a0d6
Terje Sandstrom <terje@hermit.no>
2016-01-12 22:32:47 +0100

    Added 4 mstest

---
+++ UnitTestProject4/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test5Secs()
+        {
+            var x = 5.6d;
+            var s = Stopwatch.StartNew();
+            s.Start();
+            while (s.ElapsedMilliseconds < 5000)
+            {
+                for (int i = 0; i < 2000000; i++)
+                {
+                    x = x * i / (Math.Sqrt(i));
+                }
+            }
+            Assert.IsTrue(true);
+        }
+    }
+}
